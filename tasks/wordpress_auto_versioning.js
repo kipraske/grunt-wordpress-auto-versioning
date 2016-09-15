008ecb4a6760c4014f64e9fd1df4ccdd4eec6560
Kristofer Raske <kristofer.raske@wustl.edu>
2016-09-15 15:34:07 -0500

    Initial Commit
    Running scaffolding from http://gruntjs.com/creating-plugins gives us
    this initial output

---
+++ tasks/wordpress_auto_versioning.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-wordpress-auto-versioning
+ * https://github.com/kipraske/grunt-wordpress-auto-versioning
+ *
+ * Copyright (c) 2016 Kristofer Raske
+ * Licensed under the GPLv3 license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('wordpress_auto_versioning', 'Adds the current commit number to the version of a wordpress theme. Useful for development keeping track of which version is on which environment while we are in alpha.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
